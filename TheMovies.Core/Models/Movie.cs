5f88dded4b0211967e3be6a4d1ab2a8c15da1675
lbbe79127 <lbbe79127@edu.ucl.dk>
2026-08-20 13:58:25 +0200

    Add project files.

---
+++ TheMovies.Core/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheMovies.Core.Models
+{
+    class Movie
+    {
+    }
+}
---
+++ TheMovies.UI/ViewModels/RelayCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheMovies.UI.ViewModels
+{
+    class RelayCommand
+    {
+    }
+}
