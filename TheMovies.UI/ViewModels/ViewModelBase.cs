a41b395ce59d37f3768e85d03447052f975eb644
lbbe79127 <lbbe79127@edu.ucl.dk>
2026-08-24 14:14:58 +0200

    RelayCommand and ViewModelBase set up

--- TheMovies.UI/ViewModels/ViewModelBase.cs
+++ TheMovies.UI/ViewModels/ViewModelBase.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace TheMovies.UI.ViewModels
 {
-    class ViewModelBase
+    class ViewModelBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
