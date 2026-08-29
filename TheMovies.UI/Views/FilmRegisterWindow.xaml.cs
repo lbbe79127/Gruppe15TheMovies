a97219492dcc372375c08f94056da939f7760012
benjampin <benjampin@gmail.com>
2026-08-29 10:09:27 +0200

    Integrate FilmRegisterWindow with ViewModel and Repository
    Refactor FilmRegisterWindow to use FilmRegisterWindowViewModel with IMovieRepository.

--- TheMovies.UI/Views/FilmRegisterWindow.xaml.cs
+++ TheMovies.UI/Views/FilmRegisterWindow.xaml.cs
@@ -8,28 +8,25 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows;
+using TheMovies.Core.Repositories;
+using TheMovies.UI.ViewModels;
 
 namespace TheMovies.UI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class FilmRegisterWindow : Window
     {
         public FilmRegisterWindow()
         {
             InitializeComponent();
-        }
 
-        private void btnRegister_Click(object sender, RoutedEventArgs e)
-        {
+            IMovieRepository movieRepository = new FileMovieRepository();
 
+            DataContext = new FilmRegisterWindowViewModel(movieRepository);
         }
 
         private void btnAnnuller_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Go back to Menu.");
-            // Environment.Exit(0);
             this.Close();
         }
     }
